5c00aa5c0f01f28ed2f6f036781d319a7b068f03
Borys Denysenko <48831975+IamHatefull@users.noreply.github.com>
2023-04-28 23:33:56 +0300

    find, send, refresh, wait until

---
+++ lesson18_Selenium/get_active_element.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import wait
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+#Solution to avoid error: "Couldn't read tbsCertificate as SEQUENCE"
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+#driver creating
+#Path to chromedriver.exe -> 'E:\\Downloads\\chromedriver_win32\\chromedriver.exe'
+driver = webdriver.Chrome(executable_path='E:\\Downloads\\chromedriver_win32\\chromedriver.exe', options=options)
+#driver = webdriver.Chrome()
+
+# Navigate to url
+driver.get("https://www.google.com")
+driver.find_element(By.CSS_SELECTOR, '[name="q"]').send_keys("webElement")
+
+# Get attribute of current active element
+attr = driver.switch_to.active_element.get_attribute("title")
+print(attr)
+print(driver.current_url)
+
+# Store 'SearchInput' element
+SearchInput = driver.find_element(By.NAME, "q")
+SearchInput.send_keys("selenium")
+
+# Clears the entered text
+SearchInput.clear()
+
+# Returns true if element is enabled else returns false
+value = driver.find_element(By.NAME, 'btnK').is_enabled()
+print(value)
+
+# Refresh the current page
+driver.refresh()
+
+driver.get("https://www.selenium.dev/documentation/webdriver/interactions/alerts/")
+
+# Click the link to activate the alert
+driver.find_element(By.LINK_TEXT, "See an example alert").click()
+
+# Wait for the alert to be displayed and store it in a variable
+alert = WebDriverWait.until(expected_conditions.alert_is_present())
+
+# Store the alert text in a variable
+text = alert.text
+
+# Press the OK button
+alert.accept()
+
--- lesson18_Selenium/selenium_file_upload.py
+++ lesson18_Selenium/selenium_file_upload.py
@@ -6,10 +6,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.implicitly_wait(10)
 
+#Get to the page
 driver.get("https://the-internet.herokuapp.com/upload")
+
+# find file upload element and send file by path.
 driver.find_element(By.ID,"file-upload").send_keys("E:\Python\GitHub_Repositories\GitHub\GoIT-Python-Web\lesson18_Selenium\shotgun.jpg")
 driver.implicitly_wait(10)
+
+#After upload, find submit button and press it.
 driver.find_element(By.ID,"file-submit").submit()
+
 
 if(driver.page_source.find("File Uploaded!")):
     print("File upload success")
